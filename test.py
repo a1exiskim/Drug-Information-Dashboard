fb7b698d17d94e6b3c12bcc4531dde63733a8d8f
Alexis Kim <alexiskim121@gmail.com>
2026-08-01 22:21:21 -0400

    created fake data for testing

---
+++ test.py
@@ -0,0 +1,15 @@
+from transform import clean_drug_data
+
+# create fake data 
+
+fake_drug = {
+    'openfda': {
+        'brand_name': ['fake drug'],
+        'generic_name': ['fake ingredient'],
+        'manufacturer_name': ['fake company']
+    },
+    'warnings': ['fake warning'],
+    'purpose': ['fake purpose'],
+    'dosage_and_administration': ['fale dosage and administration'],
+    'active_ingredient': ['fake ingredients']
+}

0a15fcaf22f3a827ac71fa584083029e95823fa8
Alexis Kim <alexiskim121@gmail.com>
2026-08-01 22:02:41 -0400

    update cleaning drug data

--- transform.py
+++ transform.py
@@ -2,11 +2,19 @@ def clean_drug_data(drug):
     drug_name = drug['openfda']['brand_name']
     generic_name = drug['openfda']['generic_name']
     manufacturer_name = drug['openfda']['manufacturer_name']
+    warnings = drug['warnings']
+    dosage_and_administration = drug['dosage_and_administration']
+    drug_purpose = drug['purpose']
+    active_ingredients = drug['active_ingredient']
 
     cleaned_drug = {
         'drug_name': drug_name,
         'generic_name': generic_name,
-        'manufacturer': manufacturer_name 
+        'manufacturer': manufacturer_name,
+        'warnings': warnings,
+        'purpose': drug_purpose,
+        'dosage_and_administration': dosage_and_administration, 
+        'active_ingredients': active_ingredients
     }
 
     return cleaned_drug

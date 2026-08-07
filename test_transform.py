18709a75401d0b366849dc346c3d9baf11c65d83
Alexis Kim <alexiskim121@gmail.com>
2026-08-06 21:47:47 -0400

    handle missing API field and added test

--- test_transform.py
+++ test_transform.py
@@ -1,8 +1,9 @@
+import pytest
 from transform import clean_drug_data
 
 # create fake data 
 
-fake_drug = {
+fake_drug_1 = {
     'openfda': {
         'brand_name': ['fake drug'],
         'generic_name': ['fake ingredient'],
@@ -14,31 +15,47 @@ fake_drug = {
     'active_ingredient': ['fake ingredients']
 }
 
+fake_drug_2 = {
+    'openfda': {
+        'generic_name': ['fake ingredient'],
+        'manufacturer_name': ['fake company']
+    },
+    'warnings': ['fake warning'],
+    'purpose': ['fake purpose'],
+    'dosage_and_administration': ['fake dosage and administration'],
+    'active_ingredient': ['fake ingredients']
+    }
+
 def test_brand_name():
-    cleaned_drug_data = clean_drug_data(fake_drug)
+    cleaned_drug_data = clean_drug_data(fake_drug_1)
     assert cleaned_drug_data['drug_name'] == ['fake drug']
 
 def test_generic_name():
-    cleaned_drug_data = clean_drug_data(fake_drug)
+    cleaned_drug_data = clean_drug_data(fake_drug_1)
     assert cleaned_drug_data['generic_name'] == ['fake ingredient']
 
 def test_manufacturer_name():
-    cleaned_drug_data = clean_drug_data(fake_drug)
+    cleaned_drug_data = clean_drug_data(fake_drug_1)
     assert cleaned_drug_data['manufacturer'] == ['fake company']
 
 
 def test_warnings():
-    cleaned_drug_data = clean_drug_data(fake_drug)
+    cleaned_drug_data = clean_drug_data(fake_drug_1)
     assert cleaned_drug_data['warnings'] == ['fake warning']
 
 def test_purpose():
-    cleaned_drug_data = clean_drug_data(fake_drug)
+    cleaned_drug_data = clean_drug_data(fake_drug_1)
     assert cleaned_drug_data['purpose'] == ['fake purpose']
 
 def test_dosage_and_administration():
-    cleaned_drug_data = clean_drug_data(fake_drug)
+    cleaned_drug_data = clean_drug_data(fake_drug_1)
     assert cleaned_drug_data['dosage_and_administration'] == ['fake dosage and administration']
 
 def test_active_ingredient():
-    cleaned_drug_data = clean_drug_data(fake_drug)
+    cleaned_drug_data = clean_drug_data(fake_drug_1)
     assert cleaned_drug_data['active_ingredient'] == ['fake ingredients']
+
+def test_empty_brand_name():
+    with pytest.raises(KeyError):
+        cleaned_drug_data = clean_drug_data(fake_drug_2)
+        

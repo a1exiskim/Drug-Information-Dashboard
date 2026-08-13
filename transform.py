d7cf60642e8e882589ebe2ba5eef7eb2b0206416
Alexis Kim <alexiskim121@gmail.com>
2026-08-13 17:29:07 -0400

    improve drug data transformation and validation

--- transform.py
+++ transform.py
@@ -2,16 +2,41 @@ def clean_drug_data(drug):
     default = 'No information available'
 
     try:
-        drug_name = drug['openfda']['brand_name']
+        drug_name = drug['openfda']['brand_name'] # nothing can be displayed without drug name
     except KeyError:
-        raise KeyError('Drug name missing from OpenFDA response')
+            raise KeyError('Brand name missing from OpenFDA response')
+    if drug_name:
+        drug_name = drug_name[0] # removing list structure 
+    else:
+        raise KeyError('Brand name missing from OpenFDA response')
+    
+    generic_name = drug['openfda'].get('generic_name')
+    if generic_name:
+        generic_name = generic_name[0]
+    else:
+        generic_name = default 
 
-    generic_name = drug['openfda'].get('generic_name', [default])
-    manufacturer_name = drug['openfda'].get('manufacturer_name', [default])
+    manufacturer_name = drug['openfda'].get('manufacturer_name')
+    if manufacturer_name:
+        manufacturer_name = manufacturer_name[0]
+    else:
+        manufacturer_name = default
+
     warnings = drug.get('warnings', [default])
+    if warnings == []:
+        warnings = [default] 
+    
     dosage_and_administration = drug.get('dosage_and_administration', [default])
+    if dosage_and_administration == []:
+        dosage_and_administration = [default]
+
     drug_purpose = drug.get('purpose', [default])
+    if drug_purpose == []:
+        drug_purpose = [default]
+
     active_ingredient = drug.get('active_ingredient', [default])
+    if active_ingredient == []:
+        active_ingredient = [default]
 
     cleaned_drug = {
         'drug_name': drug_name,

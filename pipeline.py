bf4d181df7c368e4087d0739afba08e827b514cc
Alexis Kim <alexiskim121@gmail.com>
2026-08-16 19:57:54 -0400

    handle missing API response

--- pipeline.py
+++ pipeline.py
@@ -4,13 +4,16 @@ from api import get_drug_data
 
 def find_or_fetch_drug(drug_name, connection):
     '''look for drug in database, if it does not exist, source from API and add to database'''
-    
+
     drug_result = get_drug(drug_name, connection)
 
     if drug_result is not None:
         return drug_result
     else:
         raw_drug_data = get_drug_data(drug_name)
+        if raw_drug_data is None:
+            return None 
+        
         drug = clean_drug_data(raw_drug_data)
         load_drug(drug, connection)
 

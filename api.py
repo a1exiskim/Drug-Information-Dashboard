796eb174d4378c4f2cb8a13e12d0843b2b15e7be
Alexis Kim <alexiskim121@gmail.com>
2026-08-16 19:51:59 -0400

    handle missing drugs in OpenFDA response

--- api.py
+++ api.py
@@ -3,8 +3,12 @@ import requests
 def get_drug_data(drug_name):
     url = f'https://api.fda.gov/drug/label.json?search=openfda.brand_name:{drug_name}'
 
-    response = requests.get(url)
+    response = requests.get(url)    
+    
     data = response.json()
+    result = data.get('results')
 
-    return data
+    if result:
+        return result[0]
 
+    return None 

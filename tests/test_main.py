dfcb576959ffe1101bb8b5efe2df56197e0ff20d
Alexis Kim <alexiskim121@gmail.com>
2026-08-16 21:25:19 -0400

    add endpoint test

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,12 @@ def test_drug_not_found():
 
     assert response.status_code == 404
     assert response.json() == {"detail": "Drug not found"}
+
+def test_drug_found():
+    response = client.get("/drugs/Tylenol")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["drug_name"] == "TYLENOL Extra Strength"

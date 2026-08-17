6403a9dbe24f63d456d54dac19a68cea47ebe662
Alexis Kim <alexiskim121@gmail.com>
2026-08-16 20:56:12 -0400

    fix connection closure

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def get_drug_endpoint(drug_name: str):
     connection = connect_to_db()
     obtained = find_or_fetch_drug(drug_name, connection)
 
+    connection.close()
+
     if obtained is None:
         raise HTTPException(status_code=404, detail="Drug not found")
-
-    connection.close()
 
     return obtained
 
